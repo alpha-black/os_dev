01936c22f9ccac1c46c790986dbd937c11d57c4c
Rohan Krishnakumar <rohan@dsl-hkibng41-54f85f-126.dhcp.inet.fi>
2017-09-11 00:21:58 +0300

    Bootloader, asm

---
+++ loader.s
@@ -0,0 +1,17 @@
+global loader           ; Entery symbol for ELF
+
+MAGIC_NUMBER equ 0x1BADB002
+FLAGS        equ 0x0            ; multiboot flags
+CHECKSUM     equ -MAGIC_NUMBER  ; All three sum up to 0
+
+section .text:
+align 4
+    dd MAGIC_NUMBER
+    dd FLAGS
+    dd CHECKSUM
+
+loader:                         ; entry point, defined in the linker
+    mov eax, 0xCAFEBABE
+
+.loop:
+    jmp .loop
