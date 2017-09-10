589ea8d8b7eb3730759fb48dd5dfc3aa28e41bf5
Rohan Krishnakumar <rohan@dsl-hkibng41-54f85f-126.dhcp.inet.fi>
2017-09-11 00:38:07 +0300

    Switching to ubuntu

--- loader.s
+++ loader.s
@@ -1,8 +1,8 @@
-global loader           ; Entery symbol for ELF
+global loader                       ; Entery symbol for ELF
 
 MAGIC_NUMBER equ 0x1BADB002
-FLAGS        equ 0x0            ; multiboot flags
-CHECKSUM     equ -MAGIC_NUMBER  ; All three sum up to 0
+FLAGS        equ 0x0                ; multiboot flags
+CHECKSUM     equ -MAGIC_NUMBER      ; All three sum up to 0
 
 section .text:
 align 4
@@ -10,7 +10,7 @@ align 4
     dd FLAGS
     dd CHECKSUM
 
-loader:                         ; entry point, defined in the linker
+loader:                             ; entry point, defined in the linker
     mov eax, 0xCAFEBABE
 
 .loop:
